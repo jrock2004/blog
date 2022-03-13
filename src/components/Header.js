e2f7f02ea179f590069530fbdf60acfa6be34ace
John Costanzo <jrock2004@gmail.com>
2022-03-13 16:37:19 -0400

    fix: fixing naming of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,3 +17,5 @@ export const Header = ({ activePath, title }) => {
     </header>
   )
 }
+
+export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { Header } from "./header"
+import Header from "./Header"
 import Bio from "./bio"
 
 const Layout = ({ location, title, children }) => {
