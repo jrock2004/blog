45e6be21b5bfe909b919627930f0d203526ede4a
John Costanzo <jrock2004@gmail.com>
2023-01-03 23:19:52 -0500

    chore: adding base of new blog

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'media',
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#d19a66',
+      },
+    },
+    plugins: [],
+  }
+}
