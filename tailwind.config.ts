dac8ebd178d3e180fb62458a3f5238d97cf51fb8
John Costanzo <jrock2004@gmail.com>
2024-05-05 15:53:30 -0400

    setting up tailwind theme and prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        text: 'var(--theme-text)',
+        heading: 'var(--theme-heading)',
+        hr: 'var(--theme-hr)',
+        layer1: 'var(--theme-layer1)',
+        layer2: 'var(--theme-layer2)',
+        layer3: 'var(--theme-layer3)',
+        muted1: 'var(--theme-muted1)',
+        muted2: 'var(--theme-muted2)',
+        muted3: 'var(--theme-muted3)',
+        primary: 'var(--theme-primary)',
+        primaryAccent: 'var(--theme-primary-accent)',
+        secondary: 'var(--theme-secondary)',
+        secondaryAccent: 'var(--theme-secondary-accent)',
+        critical: 'var(--theme-critical)',
+        criticalAccent: 'var(--theme-critical-accent)',
       },
     },
   },
